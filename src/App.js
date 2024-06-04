4684ed18a879eed18628f2575319ebada2b6751a
munaAbdullah <135506882+munaAbdullah@users.noreply.github.com>
2024-06-04 23:10:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App() {
           if (stuName) {
             return <div className="card text-center">
               <div className="card-body align-items-center justify-content-center">
-                <img src={certPath} alt ="certimge" width="60%" height="600" ></img>
+                <img src={certPath} alt ="certimge" width="50%" height="600" ></img>
                 <h5 className="card-title ">{stuName}</h5>
               </div>
             </div>;
